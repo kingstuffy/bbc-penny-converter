0a9f1fb52cbd10ab4dd76b131e6b4dc50572dc0d
Olawale Malomo <warlay234@gmail.com>
2017-07-26 23:32:23 +0100

    Improve success message style

--- js/app.js
+++ js/app.js
@@ -50,7 +50,8 @@
             var resultsMessage = composeResultsMessage(requiredCoins);
             var state = requiredCoins.length > 1 ? ' are ' : ' is ';
             var successMessage = 'The minimum number of Sterling coins needed to make '
-                + input + state + '<span class="text--results">' + resultsMessage + '</span>';
+                + '<span class="text--results">' +  input + '</span>' + state
+                + '<span class="text--results">' + resultsMessage + '</span>';
             showSuccessMessage(successMessage);
 
         }
