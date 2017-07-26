768652b5d3e7e94ff1204b80697ba253efc211e3
Olawale Malomo <warlay234@gmail.com>
2017-07-26 14:17:26 +0100

    Set up test files

---
+++ js/app.test.js
@@ -0,0 +1,23 @@
+var expect = chai.expect;
+
+describe("App", function() {
+
+    describe("#convertToPennies", function() {
+        it("should pass", function() {
+            expect(true).to.equal(true);
+        });
+    });
+
+    describe("#getDenominationQuantity", function() {
+        it("should pass", function() {
+            expect(true).to.equal(true);
+        });
+    });
+
+    describe("#getRequiredNumberOfCoins", function() {
+        it("should pass", function() {
+            expect(true).to.equal(true);
+        });
+    });
+
+});
